3093490dc9ea1ab1a7e05868d58e5e6a3a6353e4
Dan F <dan.baicoianu@endava.com>
2025-09-14 14:47:16 +0300

    Selenium-Grid-Cloud | Commit 2 | Cloud - SauceLabs

--- src/main/java/com/practicetestautomation/base/BaseTest.java
+++ src/main/java/com/practicetestautomation/base/BaseTest.java
@@ -1,16 +1,17 @@
 package com.practicetestautomation.base;
 
-import java.lang.reflect.Method;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
+import java.io.File;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.util.Properties;
+
+@Listeners({SauceLabsTestListener.class})
 public class BaseTest {
 
     protected WebDriver driver;
@@ -20,7 +21,9 @@ public class BaseTest {
     @BeforeMethod(alwaysRun = true)
     public void setUp(Method method, @Optional("chrome") String browser, @Optional("local") String environment, @Optional("Windows 11") String platform, ITestContext ctx) {
         log = LogManager.getLogger(ctx.getCurrentXmlTest().getSuite().getName());
+        setProperties();
 
+        ctx.setAttribute("sauce", false);
         switch (environment) {
             case "local":
                 driver = new BrowserDriverFactory(browser, log).createDriver();
@@ -30,12 +33,30 @@ public class BaseTest {
                 driver = new GridFactory(browser, platform, log).createDriver();
                 break;
 
+            case "sauce":
+                ctx.setAttribute("sauce", true);
+                String sauceTestName = ctx.getName() + " | " + method.getName() + " | " + browser + " | " + platform;
+                SauceLabsFactory factory = new SauceLabsFactory(browser, platform, log, sauceTestName);
+                driver = factory.createDriver();
+                ctx.setAttribute("sessionId", factory.getSessionId());
+                break;
+
             default:
                 driver = new BrowserDriverFactory(browser, log).createDriver();
                 break;
         }
 
         driver.manage().window().maximize();
+    }
+
+    private void setProperties() {
+        Properties props = System.getProperties();
+        try {
+            props.load(Files.newInputStream(new File("src/main/resources/test.properties").toPath()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
     @AfterMethod(alwaysRun = true)
